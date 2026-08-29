6e270b649368eef78908eb3fcbe2f0bfe6fcb167
Woson. <779850604@qq.com>
2026-08-29 23:05:31 +0800

    Reapply "添加项目文件。"
    This reverts commit e410b54eeed8ad0f8d5151b643aa4e498e354d79.

---
+++ Models/FeatureConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+namespace HalconWithCsharp_CarCard.Models
+{
+    public class FeatureConverter:StringConverter
+    {
+        public override bool GetStandardValuesSupported(ITypeDescriptorContext? context)
+        {
+            return true;
+        }
+
+        public override bool GetStandardValuesExclusive(ITypeDescriptorContext? context)
+        {
+            return true;
+        }
+
+        public override StandardValuesCollection? GetStandardValues(ITypeDescriptorContext? context)
+        {
+            var list = new List<string> { "area", "row","column","width","height"};
+            return new StandardValuesCollection(list);
+        }
+    }
+}
---
+++ Models/Threshold.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+namespace HalconWithCsharp_CarCard.Models
+{
+    public class Threshold
+    {
+        [DisplayName("最小灰度值")]
+        public int mingray { get; set; } = 0;
+        [DisplayName("最大灰度值")]
+        public int maxgray { get; set; } = 80;
+    }
+}
